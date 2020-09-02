0d57c937b893d9f51cd9f926d09aceacf2a7b78f
Taeyoon <naming0617@gmail.com>
2020-09-03 00:51:59 +0900

    프로젝트 초기 셋팅

---
+++ src/test/java/me/naming/onlineshoppingmall/OnlineShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package me.naming.onlineshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingMallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
